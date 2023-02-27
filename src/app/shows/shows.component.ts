7901c429e19c8a4ca2cf4c1da8aebd32e9d1255f
Hugo Otth <1968678@cegepmontpetit.ca>
2023-02-27 14:54:45 -0500

    Zoom et center custom

--- src/app/shows/shows.component.ts
+++ src/app/shows/shows.component.ts
@@ -1,9 +1,7 @@
 import { BandInTownService } from './../services/BandInTown.service';
 import { Show } from './../models/show';
 import { Component, OnInit } from '@angular/core';
-import { Artist } from './../models/artist';
 import { ActivatedRoute } from '@angular/router';
-import { Album } from '../models/album';
 import { SpotifyService } from '../services/spotify.service';
 
 @Component({
@@ -15,7 +13,7 @@ export class ShowsComponent implements OnInit {
   
   artistName : string | null = null;
   center : google.maps.LatLngLiteral = {lat:42, lng:-45}
-  zoom : number = 3;
+  zoom : number = 10;
   markerPositions : google.maps.LatLngLiteral[] = [];
   shows : Show[] = [];
 
@@ -25,7 +23,6 @@ export class ShowsComponent implements OnInit {
     this.artistName = this.route.snapshot.paramMap.get("artistName");
     await this.getShows();
     this.addMarkers();
-    console.log(this.markerPositions.length)
   }
 
   async getShows() : Promise<void>{
@@ -33,9 +30,28 @@ export class ShowsComponent implements OnInit {
   }
 
   addMarkers() : void{
+    let arrLat : number[] = [];
+    let arrLng : number[] = [];
     this.shows.forEach(show => {
       this.markerPositions.push({lat: show.lat, lng: show.lng});
+      arrLat.push(show.lat);
+      arrLng.push(show.lng);
     });
+    let arrMidLat : number[] = [];
+    let arrMidLng : number[] = [];
+    arrMidLat.push(Math.max(...arrLat));
+    arrMidLat.push(Math.min(...arrLat));
+    arrMidLng.push(Math.max(...arrLng));
+    arrMidLng.push(Math.min(...arrLng));
+    let midLat = arrMidLat.reduce((a, b) => a + b, 0) / arrMidLat.length;
+    let midLng = arrMidLng.reduce((a, b) => a + b, 0) / arrMidLng.length;
+    this.center = {lat:midLat, lng:midLng}
+    console.log(Math.max(...arrLng)- Math.min(...arrLng));
+    if(arrLng.length < 2){
+      this.zoom = 10;
+    }else{
+      this.zoom = Math.abs(-0.010309278350515464 * (Math.max(...arrLng)- Math.min(...arrLng)) +4.257731958762887);
+    }
+    console.log(this.zoom);
   }
-
 }
