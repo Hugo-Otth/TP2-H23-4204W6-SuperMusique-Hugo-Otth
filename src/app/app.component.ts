97d908dd3c92f8b7e081cdd130d77584375a5452
Hugo Otth <1968678@cegepmontpetit.ca>
2023-03-02 14:52:33 -0500

    CONFIG Avencement traduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TP2-H23-4204W6-SuperMusique-Hugo-Otth';
+  language : string = "fr";
+
+  constructor(public translator : TranslateService) {
+    translator.setDefaultLang(this.language);
+  }
+
+  changeLanguage():void{
+    this.translator.use(this.language);
+  }
 }
